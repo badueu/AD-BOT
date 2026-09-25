a562defd6a734bdd7828b4dee60eda98fccc8320
badueu <arta2ohwo@gmail.com>
2026-09-25 20:17:37 +0330

    Jteuklgxdi
    Bbb

--- adbot.py
+++ adbot.py
@@ -2713,8 +2713,8 @@ async def main():
     from http.server import BaseHTTPRequestHandler, HTTPServer
     
     logger.info("تلاش برای بارگذاری متغیرهای محیطی...")
-    room_id = os.getenv("ROOM_ID", "6a2d39afaaf596f50c350fa7")
-    api_token = os.getenv("API_TOKEN", "5349cb727ef0f501c5ce5f7da95d03386f79c09c3ef1baac3345fccb8ea9130b")
+    room_id = os.getenv("ROOM_ID", "6ab0f2bffcc2d3aaff6fe97b")
+    api_token = os.getenv("API_TOKEN", "0f379262be9513009a520c3b62dca3dbf84811893b74c606221154946df0c606")
     
     if not room_id or not api_token:
         logger.error("ROOM_ID یا API_TOKEN تنظیم نشده‌اند.")
