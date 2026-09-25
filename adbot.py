b4471757875d2bb0c3e7f91f8d191ed829ea00f3
badueu <arta2ohwo@gmail.com>
2026-09-25 18:52:59 +0330

    Sjegkwwn
    Bbb

--- adbot.py
+++ adbot.py
@@ -2714,7 +2714,7 @@ async def main():
     
     logger.info("تلاش برای بارگذاری متغیرهای محیطی...")
     room_id = os.getenv("ROOM_ID", "68e771922d585712212e8070")
-    api_token = os.getenv("API_TOKEN", "ed578ca3bb6a6592059a878e4d807d0c7eac56742239fdcce7ca84841f2437e5")
+    api_token = os.getenv("API_TOKEN", "5349cb727ef0f501c5ce5f7da95d03386f79c09c3ef1baac3345fccb8ea9130b")
     
     if not room_id or not api_token:
         logger.error("ROOM_ID یا API_TOKEN تنظیم نشده‌اند.")
